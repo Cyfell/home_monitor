1dca4356943bbae312f417d57f8f550b65613235
Arthur Courtel <arthurcourtel@gmail.com>
2018-10-24 00:15:08 +0200

    save

--- tram.php
+++ tram.php
@@ -6,6 +6,12 @@
 
 <div class="container">
   <h1>Tram</h1>
+  <?php
+  $query = http_build_query(array('search'=>$as_any, 'location'=>$location));
+  $url = "https://google.com" . $query;
+  $my_var = file_get_contents($url);
+  var_dump($my_var);
+  ?>
 </div>
 
 </body>
