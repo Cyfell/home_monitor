02c2e444728126d54eea4b65e3d955fa2d60b841
Arthur Courtel <Arthur@linux.home>
2018-10-23 23:23:26 +0200

    Add base site struct

---
+++ elec.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include "header.html"; ?>
+<?php include "navbar.html"; ?>
+<body>
+
+<div class="container">
+  <h1>Elec</h1>
+</div>
+
+</body>
+<?php include "footer.html"; ?>
+</html>
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include "header.html"; ?>
+<?php include "navbar.html"; ?>
+<body>
+
+<div class="container">
+  <h1>My First Bootstrap Page</h1>
+  <p>This part is inside a .container class.</p>
+  <p>The .container class provides a responsive fixed width container.</p>
+</div>
+
+</body>
+<?php include "footer.html"; ?>
+</html>
